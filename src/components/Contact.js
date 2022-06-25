8de7efa15b7c23de4b99ee06511b15220920bef6
USH123 <unggulsenohandaru@yahoo.com>
2022-06-26 02:12:06 +0700

    feat: first-try

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,22 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import './Contact.css'
 
-const Contact = () => {
+const Contact = (props) => {
     return (
-        <></>
+        <>
+        <div className='content-grid' style={{ }}>
+            <div className='left-section'>
+                <img src={props.contact.photo} className = "profile-picture" ></img>
+            </div>
+            <div className = "right-section" style={{  }}>
+                <p>{props.contact.name}</p>
+                <p>{props.contact.phone}</p>
+                <p>{props.contact.email}</p>
+            </div>
+        </div>
+        </>
     )
 }
 
