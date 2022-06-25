cab479c8224d391af9dd522123a82e254498c79f
USH123 <unggulsenohandaru@yahoo.com>
2022-06-26 02:19:19 +0700

    feat: second-try

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import './App.css';
-import Contact from './components/Contact';
-import Header from './components/Header';
+import "./App.css";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
 
 // Uncomment untuk memuat daftar kontak
-import contacts from './data/contacts.json';
-
-
+import contacts from "./data/contacts.json";
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
   return (
     <div className="App">
       <Header />
-
-      {contacts.map(element => (
-        <Contact contact={element}/>
-      ))}
+      <Contact contact={contacts[0]} />
+      <Contact contact={contacts[1]} />
+      <Contact contact={contacts[2]} />
+      <Contact contact={contacts[3]} />
+      <Contact contact={contacts[4]} />
+      <Contact contact={contacts[5]} />
     </div>
   );
-}
+};
 
 export default App;
